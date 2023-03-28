af660756f44e2a2806293f0fe0cac79f1822ea8c
joseescoli <jose.a.escoli@gmail.com>
2023-03-28 13:20:40 -0300

    Agregado de logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,20 @@
 import '../App.css';
+import logo from './escudo.png';
+
 import { CartWidget } from "../CartWidget/CartWidget"
 
 export const NavBar = () => {
    return (
-    <nav className='navbar'>
-        {/* <img src="./escudo.png" alt="Logo" /> */}
-        <ul>
-            <li>Niños</li>
-            <li>Adolescentes</li>
-            <li>Adultos</li>
-            <li>First</li>
-            <li>Acelerados</li>
-        </ul>
-        <CartWidget />
-    </nav>
+        <nav className='navbar'>
+            <img src={logo} className='logo' alt="Logo" />
+            <ul>
+                <li>Niños</li>
+                <li>Adolescentes</li>
+                <li>Adultos</li>
+                <li>First</li>
+                <li>Acelerados</li>
+            </ul>
+            <CartWidget />
+        </nav>
    )
 }
